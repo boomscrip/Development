08ff19d5bb0ad56b5af5d2ef9c76ef716ba0708f
alex <boomscrip@gmail.com>
2019-10-10 16:00:24 +0300

    first commit

---
+++ script/typing-text.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#typing', {
+    strings: ["Создание сайта", "Продвижение сайта"],
+    loop: true,
+    smartBackspace: true,
+    typeSpeed: 40,
+    startDelay: 500,
+    backDelay: 700,
+    content: 'html',
+});
